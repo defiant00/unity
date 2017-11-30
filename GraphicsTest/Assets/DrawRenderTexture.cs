37f972ffaed869411e2303ad477b3414baaaad01
Aaron Mady <defiant00@gmail.com>
2017-11-29 20:01:27 -0500

    GraphicsTest initial commit

---
+++ GraphicsTest/Assets/DrawRenderTexture.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DrawRenderTexture : MonoBehaviour
+{
+	public RenderTexture RTex;
+	public Material Mat;
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		Graphics.Blit(source, destination);
+		Graphics.Blit(RTex, destination, Mat);
+	}
+}
