a76b5bc36e8dac23453cf9968bfdb4b42767f7ee
Aaron Mady <defiant00@gmail.com>
2018-02-11 20:15:14 -0500

    initial version

---
+++ HerdingCats/Assets/Scripts/Controls.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+	const float SPEED = 30;
+
+	void Update()
+	{
+		transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * SPEED;
+	}
+}
